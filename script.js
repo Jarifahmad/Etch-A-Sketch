29f4acd50f796286f03437bc3cdd46d63befea74
Jarif Ahmad <jarifahmad@yahoo.com>
2021-11-25 18:13:18 +0000

    updated

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const btnRGB = document.createElement(`button`)
 const btnSize = document.createElement(`button`);
 const buttonsContainer = document.querySelector(`buttons`);
 
+
+window.onload = () => {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => box.addEventListener('mouseover', () => {
+        box.style.background = 'black'
+    }))
+}
+
+
 function createDivs(col, rows){
     for(let i = 0; i < (col * rows); i++){
         const div = document.createElement(`div`)
