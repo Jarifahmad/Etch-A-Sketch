0fbec749970ce3ffc95bc6a3aa3bdf71623ae4c6
Jarif Ahmad <jarifahmad@yahoo.com>
2021-11-25 18:14:11 +0000

    updated

--- script.js
+++ script.js
@@ -14,13 +14,12 @@ window.onload = () => {
 }
 
 
-function createDivs(col, rows){
-    for(let i = 0; i < (col * rows); i++){
-        const div = document.createElement(`div`)
-        div.style.border = `1px solid red`
-        container.style.gridTemplateColumns = `repeat(${col}, 1fr`;
-        container.style.gridTemplateRows = `repeat(${rows}, 1fr`;
-        container.appendChild(div).classList.add(`box`)
+function creatDivs(col , rows) {
+    for(let i = 0;i < (col * rows); i++) {
+        const div = document.createElement('div') 
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.appendChild(div).classList.add('box')
     }
 }
 
