afcf92db6abc6f1e75c7947e9d1f5b59587a4264
Jarif Ahmad <jarifahmad@yahoo.com>
2021-11-26 23:08:04 +0000

    updated

--- script.js
+++ script.js
@@ -22,23 +22,20 @@ function creatDivs(col , rows) {
         container.appendChild(div).classList.add('box')
     }
 }
-
-createDivs(16,16)
+creatDivs(16,16)
 
 function grayColor() { 
     
     const boxs = container.querySelectorAll('.box')
-    btnRgb.textContent = "RGB"
-    btnRgb.addEventListener('click' ,() => {
-        boxs.forEach(box => box.addEventListener('mouseover', () => {
-            let R = Math.floor(Math.random() * 256);
-            let G = Math.floor(Math.random() * 256);
-            let B = Math.floor(Math.random() * 256);
-            const RGB = `rgb(${R},${G},${B})`;
-            box.style.background = RGB;
+    btnGray.textContent = "GRAY"
+    btnGray.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let RNum = Math.floor(Math.random() * 256);
+            let GrayScale = `rgb(${RNum},${RNum},${RNum})`
+            box.style.background = GrayScale;
         }))
     })
-    buttonsContainer.appendChild(btnRgb).classList.add('btn')
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
 }
 grayColor()
 
@@ -59,3 +56,70 @@ function rgbColors() {
     buttonsContainer.appendChild(btnRgb).classList.add('btn')
 }
 rgbColors()
+
+function blackColor() {
+   
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.textContent = 'BLACK';
+    btnBlack.addEventListener('click', function () {
+        boxs.forEach(box => box.addEventListener('mouseover', function() {
+            this.style.background = 'black'
+        }))
+    })
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
+}
+blackColor() 
+
+function reSet() {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => {
+        box.remove();
+    })
+}
+
+
+function reSize() {
+   
+    btnSize.textContent = 'GRID SIZE'
+    btnSize.addEventListener('click', () => {
+        let user = prompt('WHAT SIZE YOU WANT YOUR GRID TO BE?')
+        if(user === null || user < 1){
+            reSet();
+            creatDivs(16,16);
+            grayColor();
+            rgbColors();
+            blackColor();
+        } else { 
+            reSet();
+            creatDivs(user,user);
+            grayColor();
+            rgbColors();
+            blackColor();
+        }
+    })
+    buttonsContainer.appendChild(btnSize).classList.add('btn')
+}
+reSize()
+
+function backgrooundAnim() {
+
+    colors = ['red','green','blue', 'yellow'];
+    const section = document.querySelector('.section');
+    const span = document.createElement('span');
+
+    let size = Math.random() * 50;
+
+    span.style.width = 10 + size + 'px';
+    span.style.height = 10 + size + 'px';
+    span.style.borderRadius = `${size}%`;
+    span.style.top = Math.random() * innerHeight + "px";
+    span.style.left = Math.random() * innerWidth + "px";
+
+    const bg = colors[Math.floor(Math.random() * colors.length)];
+    span.style.background = bg;
+
+    section.appendChild(span);
+
+    setTimeout(() => {span.remove()},5000)
+}
+
